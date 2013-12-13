71916a681985407fc413fa04f1e664b174b60bf6
Max Bucknell <max.bucknell@redboxdigital.com>
2013-12-13 14:36:34 +0000

    Add some documentation.

--- app/code/community/MaxBucknell/AMDJS/Block/Modules.php
+++ app/code/community/MaxBucknell/AMDJS/Block/Modules.php
@@ -4,20 +4,34 @@ require_once(Mage::getBaseDir('lib').DS.'amd-packager-php/Packager.php');
 
 class MaxBucknell_AMDJS_Block_Modules extends Mage_Core_Block_Template
 {
-    protected $_modules;
+    /**
+     * @var array The modules that are being used in this request
+     */
+    protected $_modules = array();
 
     protected function _construct ()
     {
         parent::_construct();
-        $this->_modules = array();
         $this->setTemplate('maxbucknell/amdjs/modules.phtml');
     }
 
+    /**
+     * Add a module to this page.
+     *
+     * @param string $moduleName
+     * @return void
+     */
     public function addModule ($moduleName)
     {
         $this->_modules[$moduleName] = $moduleName;
     }
 
+    /**
+     * Stop using a module on this page.
+     *
+     * @param string $moduleName
+     * @return void
+     */
     public function removeModule($moduleName)
     {
         unset($this->_modules[$moduleName]);
--- app/code/community/MaxBucknell/AMDJS/Helper/Data.php
+++ app/code/community/MaxBucknell/AMDJS/Helper/Data.php
@@ -1,50 +1,103 @@
 <?php
+/**
+ * @author Max Bucknell <me@maxbucknell.com>
+ * @copyright 2013 Max Bucknell
+ */
 
+/**
+ * PHP implementation of the AMD specification.
+ */
 require_once(Mage::getBaseDir('lib').DS.'amd-packager-php/Packager.php');
 
+/**
+ * Functionality to compile modules with dependencies.
+ */
 class MaxBucknell_AMDJS_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    /**
+     * The directory containing the source modules
+     * @return string
+     */
     public function getSourceBaseDir()
     {
         return Mage::getBaseDir().DS.'js'.DS.'maxbucknell'.DS.'amdjs'.DS.'modules';
     }
 
+    /**
+     * The location relative to filesystem root of a module definition.
+     *
+     * @param string module name
+     * @return string
+     */
     public function getSourceFileName($module)
     {
         return $this->getSourceBaseDir().DS.$module.'.js';
     }
 
+    /**
+     * The directory containing the built files
+     * @return string
+     */
     public function getBuiltBaseDir()
     {
         return Mage::getBaseDir('media').DS.'amdjs-cache';
     }
 
+    /**
+     * The location of a set of built modules relative to the filesystem.
+     *
+     * @param array $modules
+     * @return string
+     */
     public function getBuiltFileName($modules)
     {
         return $this->getBuiltBaseDir().DS.$this->_getModuleHash($modules).'.js';
     }
 
+    /**
+     * The directory containing the source modules
+     * @return string
+     */
     public function getBuiltBaseUrl()
     {
         return Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_MEDIA).DS.'amdjs-cache';
     }
 
+    /**
+     * The URL of a set of built modules.
+     * @param array $modules
+     * @return string
+     */
     public function getBuiltFileUrl($modules)
     {
         return $this->getBuiltBaseUrl().DS.$this->_getModuleHash($modules).'.js';
     }
 
+    /**
+     * Generate a unique hash of a set of modules.
+     * @param array $modules
+     * @return string
+     */
     protected function _getModuleHash($modules)
     {
         ksort($modules);
         return md5(implode($modules));
     }
 
+    /**
+     * Create the dir containing the built modules if it does not exist.
+     * @return void
+     */
     protected function _createDir()
     {
         Mage::app()->getConfig()->getOptions()->createDirIfNotExists($this->getBuiltBaseDir());
     }
 
+    /**
+     * Compile a set of modules.
+     *
+     * @param array $modules
+     */
     protected function _build($modules)
     {
         $filename = $this->getBuiltFileName($modules);
@@ -60,6 +113,12 @@ class MaxBucknell_AMDJS_Helper_Data extends Mage_Core_Helper_Abstract
         file_put_contents($filename, $output);
     }
 
+    /**
+     * True if a set of modules has already been compiled.
+     *
+     * @param array $modules
+     * @return boolean
+     */
     public function isModuleSetCached($modules)
     {
         return false;
@@ -68,11 +127,21 @@ class MaxBucknell_AMDJS_Helper_Data extends Mage_Core_Helper_Abstract
         return $this->_loadCache($hash) !== false;
     }
 
+    /**
+     * Compile and cache a set of modules.
+     *
+     * @param array $modules
+     */
     public function cacheModuleSet($modules)
     {
         $this->_build($modules);
     }
 
+    /**
+     * Remove a set of modules from the cache.
+     *
+     * @param array $modules
+     */
     public function clearModuleSetCache($modules)
     {
         $hash = $this->_hashModuleSet($modules);
